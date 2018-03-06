a1209df75084c57217fc34bff8030f425775c520
Glitch (carto-single-layer) <none>
2018-03-06 20:30:32 +0000

    🙎📻 Checkpoint script.js:441232/325 index.html:441232/1 styles.css:441232/2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* global L, carto */
 
 const map = L.map('map').setView([30, 0], 3);
 
@@ -6,18 +7,25 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/
   maxZoom: 18
 }).addTo(map);
 
+// Initialize Carto
 const client = new carto.Client({
   apiKey: 'apikey',
   username: 'brelsfoeagain'
 });
 
+// Initialze source data
 const source = new carto.source.Dataset('hms_efh_2009tiger_shark');
+
+// Create style for the data
 const style = new carto.style.CartoCSS(`
   #layer {
     polygon-fill: red;
   }
 `);
+
+// Add style to the data
 const layer = new carto.layer.Layer(source, style);
 
+// Add the data to the map as a layer
 client.addLayer(layer);
 client.getLeafletLayer().addTo(map);
