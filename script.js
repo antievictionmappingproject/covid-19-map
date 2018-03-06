209354b58a29120a1cdfd3632d36edc5c0674d8a
Glitch (sour-pilot) <none>
2018-03-06 20:20:06 +0000

    💕🚡 Checkpoint index.html:441232/63 script.js:441232/70 styles.css:441232/69

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hi');
+
+const map = L.map('map').setView([30, 0], 3);
+
+// Add base layer
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+  maxZoom: 18
+}).addTo(map);
+
+const client = new carto.Client({
+  apiKey: 'apikey',
+  username: 'brelsfoeagain'
+});
+
+const source = new carto.source.Dataset('hms_efh_2009tiger_shark');
+const style = new carto.style.CartoCSS(`
+  #layer {
+    polygon-fill: red;
+  }
+`);
+const layer = new carto.layer.Layer(source, style);
+
+client.addLayer(layer);
+client.getLeafletLayer().addTo(map);
