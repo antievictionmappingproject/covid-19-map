4b435a5caacf82e0a91f841fae2d08adab72a36d
Glitch (leaflet-data) <none>
2018-09-02 18:24:45 +0000

    👅👫 Checkpoint ./README.md:441232/30 ./script.js:441232/961 ./index.html:441232/10 ./.glitch-assets:441232/393

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# leaflet-simple
+# leaflet-data
 
-Create a Leaflet map with a basemap.
+Create a Leaflet map with a basemap and overlay data
--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // This isn't necessary but it keeps the editor from thinking L is a typo
 /* global L */
 
-var map = L.map('map').setView([34.034453, -82.199707], 5);
+var map = L.map('map').setView([34.03, -82.20], 5);
 
 // Add base layer
 L.tileLayer(' https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
   maxZoom: 18
 }).addTo(map);
+
+// Fetch data from our Glitch project
+fetch('https://cdn.glitch.com/baade5a3-f979-48f2-9a28-14daee16fab0%2Fmap.geojson?1535912286843')
+  .then(function (response) {
+    // Read data as JSON
+    return response.json();
+  })
+  .then(function (data) {
+    // Add data to the map
+    L.geoJson(data).addTo(map);
+  });
