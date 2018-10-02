46f555aaec27dca3be5ff9ee6e1214f22cd88185
Glitch (leaflet-popups) <none>
2018-10-02 13:57:23 +0000

    👛✊ Checkpoint ./index.html:441232/665 ./script.js:441232/595

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // This isn't necessary but it keeps the editor from thinking L is a typo
-/* global L */
+/* global L, Mustache */
 
 var map = L.map('map').setView([34.03, -82.20], 5);
+
+// Get the popup template from the HTML.
+//
+// We can do this here because the template will never change.
+var popupTemplate = document.querySelector('.popup-template').innerHTML;
 
 // Add base layer
 L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
@@ -20,10 +25,11 @@ fetch('https://data.cityofnewyork.us/resource/fhrw-4uyv.geojson?$where=latitude 
     // Add popups to the layer
     complaintData.bindPopup(function (layer) {
       // This function is called whenever a feature on the layer is clicked
+      console.log(layer.feature.properties);
       
-      // Uncomment this to see all properties on the clicked feature:
-      // console.log(layer.feature.properties);
-      return layer.feature.properties['complaint_type'];
+      // Render the template with all of the properties. Mustache ignores properties
+      // that aren't used in the template, so this is fine.
+      return Mustache.render(popupTemplate, layer.feature.properties);
     });
   
     // Add data to the map
