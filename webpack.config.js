6b6f4d82e78717f9f48779540dce9b708aa12ed2
Chris Henrick <henrick@google.com>
2020-05-19 20:02:36 -0700

    updated eslint & webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -146,6 +146,7 @@ module.exports = (env, argv) => {
                   "@babel/plugin-proposal-object-rest-spread",
                   "@babel/plugin-transform-runtime",
                   "@babel/plugin-transform-async-to-generator",
+                  "@babel/plugin-proposal-class-properties"
                 ],
               },
             },
