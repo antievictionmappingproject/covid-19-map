ed26afacdf32ae6233b5fef384eb47c2bec14d61
Chris Henrick <henrick@google.com>
2020-05-20 18:27:41 -0700

    update LeafletMap component: - handle events - implement handle cities layer

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,20 +1,31 @@
-const defaultConfig = {
-  lat: 40.67,
-  lng: -97.23,
-  z: 4,
-  nations: false,
-  states: true,
-  cities: true,
-  counties: true,
-  rentStrikes: true,
-};
+import { dispatch } from "utils/dispatch";
+import {
+  defaultMapConfig,
+  colorNoData,
+  fillColorScale,
+  strokeColorScale,
+  strokeWeight,
+  pointRadius,
+  fillOpacity,
+  policyStrengthLanguage,
+  isMobile,
+} from "utils/constants";
 
 export class LeafletMap {
-  dataLayers = new Map();
+  dataLayers = new Map([
+    ["cities", null],
+    ["counties", null],
+    ["states", null],
+    ["nations", null],
+    ["rentstrikes", null],
+  ]);
+
+  popupTemplate = document.querySelector(".popup-template").innerHTML;
 
   constructor(config) {
-    this.config = config || defaultConfig;
+    this.config = config || defaultMapConfig;
     this.init();
+    this.bindListeners();
   }
 
   init() {
@@ -58,40 +69,116 @@ export class LeafletMap {
     ).addTo(this.map);
   }
 
-  handleLayersAdded() {
-    this.layersControl;
-    // .addOverlay(rentStrikes, "Rent Strikes")
-    // .addOverlay(cities, "Cities")
-    // .addOverlay(counties, "Counties")
-    // .addOverlay(states, "States")
-    // .addOverlay(nations, "Nations");
+  bindListeners() {
+    const self = this;
+    this.map.on("popupopen", function (e) {
+      document.getElementById("root").classList.add("aemp-popupopen");
+
+      if (isMobile()) {
+        dispatch.call("title-details-close");
+        self.map.invalidateSize();
+      }
+
+      self.map.setView(e.popup._latlng, self.map.getZoom(), { animate: true });
+    });
+
+    this.map.on("popupclose", function () {
+      document.getElementById("root").classList.remove("aemp-popupopen");
+      dispatch.call("close-infowindow");
+      if (isMobile())
+        setTimeout(function () {
+          self.map.invalidateSize();
+        }, 100);
+    });
+
+    this.map.on("click", function () {
+      if (isMobile()) {
+        dispatch.call("title-details-close");
+      }
+    });
+
+    let resizeWindow;
+    window.addEventListener("resize", function () {
+      clearTimeout(resizeWindow);
+      resizeWindow = setTimeout(self.handleWindowResize, 250);
+    });
+  }
+
+  handleWindowResize = () => {
+    if (isMobile()) {
+      this.map.invalidateSize();
+    }
+  };
+
+  handleCitiesLayer(geojson) {
+    const self = this;
+    const pointToLayer = (feature, latlng) => {
+      return L.circleMarker(latlng, this.getLayerStyle(feature));
+    };
+
+    const citiesLayer = L.geoJson(geojson, {
+      pointToLayer: pointToLayer,
+    });
+
+    citiesLayer.bindPopup(function (layer) {
+      // This function is called whenever a feature on the layer is clicked
+
+      // Render the template with all of the properties. Mustache ignores properties
+      // that aren't used in the template, so this is fine.
+      const { municipality, state, country } = layer.feature.properties;
+      const props = {
+        ...layer.feature.properties,
+        // Build city name with state and country if supplied
+        jurisdictionName: `${municipality}${state ? `, ${state}` : ""}${
+          country ? `, ${country}` : ""
+        }`,
+        jurisdictionType: "City",
+        popupName: municipality,
+        policyStrength: policyStrengthLanguage[layer.feature.properties.range],
+      };
+
+      dispatch.call("render-infowindow", null, {
+        template: "protections",
+        data: props,
+      });
+
+      return Mustache.render(self.popupTemplate, props);
+    });
+
+    this.dataLayers.set("cities", citiesLayer);
+
+    if (this.config.cities) {
+      citiesLayer.addTo(this.map);
+    }
+  }
+
+  handleCountiesLayer() {}
+
+  handleStatesLayer() {}
+
+  handleNationsLayer() {}
+
+  handleRentStrikeLayer() {}
+
+  handleAllLayersAdded() {
+    this.dataLayers.forEach(function (layerGroup, name) {
+      this.layersControl.addOverlay(layerGroup, name);
+    });
 
     // Apply correct relative order of layers when adding from control.
     this.map.on("overlayadd", function () {
       this.fixZOrder(this.dataLayers);
     });
+  }
 
-    // if any layers in the map config are set to false,
-    // remove them from the map
-    if (!this.mapConfig.nations) {
-      this.map.removeLayer(this.dataLayers.get("nations"));
-    }
-
-    if (!this.mapConfig.states) {
-      this.map.removeLayer(this.dataLayers.get("states"));
-    }
-
-    if (!this.mapConfig.counties) {
-      this.map.removeLayer(this.dataLayers.get("counties"));
-    }
-
-    if (!this.mapConfig.cities) {
-      this.map.removeLayer(this.dataLayers.get("cities"));
-    }
-
-    if (!this.mapConfig.rentStrikes) {
-      this.map.removeLayer(this.dataLayers.get("rentStrikes"));
-    }
+  getLayerStyle(feature) {
+    return {
+      color: strokeColorScale[feature.properties.range] || colorNoData,
+      fillColor: fillColorScale[feature.properties.range] || colorNoData,
+      fillOpacity: fillOpacity,
+      radius: pointRadius,
+      weight: strokeWeight,
+    };
   }
 
   // helper methods
@@ -101,5 +188,13 @@ export class LeafletMap {
         layerGroup.bringToBack();
       }
     });
+  }
+
+  invalidateSize() {
+    this.map.invalidateSize();
+  }
+
+  closePopup() {
+    this.map.closePopup();
   }
 }
