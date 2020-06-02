32e9c76186cc08b12a1de56787414ca1158738bb
Chris Henrick <henrick@google.com>
2020-06-02 08:50:28 -0700

    fixed regex for css & scss modules in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -97,7 +97,7 @@ module.exports = (env, argv) => {
         // this "rule" tells webpack what "loaders" to use to process our CSS
         {
           // use a Regular Expression to tell webpack what type of file(s) this rule targets
-          test: /\.(s)css$/,
+          test: /\.(css|scss)$/,
           // tell webpack what "loaders" to use to process this file type
           use: [
             {
