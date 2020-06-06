27d5e30b572518605ebd3e11dc0ab24afe0aa616
Chris Henrick <henrick@google.com>
2020-06-06 11:18:12 -0700

    added dep leaflet.pattern & include in leaflet export

--- src/lib/leaflet.js
+++ src/lib/leaflet.js
@@ -1,9 +1,9 @@
 // NOTE: use this module for referencing Leaflet,
 // so that any Leaflet plugins are also available
 const L = Object.assign(
-  {},
   require("leaflet"),
-  require("leaflet.markercluster")
+  require("leaflet.markercluster"),
+  require("leaflet.pattern")
 );
 
 export const rentStrikeIcon = new L.Icon({
