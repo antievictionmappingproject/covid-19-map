531b4737fdd38e3df07690869aee44d1fc969783
unixjazz <luisfelipe@softwarelivre.org>
2020-07-05 20:14:46 -0400

    added Spanish (es) locale.

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,9 +1,13 @@
 import en from "./en.json";
+import es from "./es.json";
 import ptBr from "./pt-BR.json";
 
 export default {
   en: {
     translation: en,
+  },
+  es: {
+    translation: es,
   },
   "pt-BR": {
     translation: ptBr,
