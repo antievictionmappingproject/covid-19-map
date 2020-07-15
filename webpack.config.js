c55db448f074d6ca9bf95149690dc213e51901f1
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-07-15 11:06:04 -0700

    update fonts, add fonts.scss

--- webpack.config.js
+++ webpack.config.js
@@ -169,14 +169,7 @@ module.exports = (env, argv) => {
         // rule to handle loading fonts
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            {
-              loader: "url-loader",
-              options: {
-                limit: 10000,
-              },
-            },
-          ],
+          use: ["file-loader"],
         },
       ],
     },
