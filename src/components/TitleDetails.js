f3940ceb4fd9b9e7189042aa7383f60137b7a89a
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-07-31 13:58:17 -0700

    hookup drawer action on titlebox

--- src/components/TitleDetails.js
+++ src/components/TitleDetails.js
@@ -4,15 +4,21 @@ import { dispatch } from "utils/dispatch";
  * TitleDetails: // the collapsable <details> element in the <header>
  */
 export class TitleDetails {
+  titleBox = document.getElementById("aemp-titlebox");
+
   titleDetails = document
     .getElementById("aemp-titlebox")
     .querySelector("details");
+
+  toggleButton = document.getElementById("aemp-titlebox-toggle");
 
   constructor() {
     this.bindListeners();
   }
 
   bindListeners = () => {
+    const self = this;
+
     this.titleDetails.addEventListener("toggle", function () {
       if (this.open) {
         document.getElementById("aemp-titlebox").classList.remove("collapsed");
@@ -21,9 +27,22 @@ export class TitleDetails {
       }
     });
 
+    this.toggleButton.addEventListener("click", function () {
+      const { className } = document.getElementById("aemp-titlebox");
+      self.toggleTitleBox(className);
+    });
+
     dispatch.on("title-details-toggle", this.toggleTitleDetails);
     dispatch.on("title-details-open", this.openTitleDetails);
     dispatch.on("title-details-close", this.closeTitleDetails);
+  };
+
+  toggleTitleBox = (className) => {
+    if (className === "aemp-titlebox-open") {
+      this.titleBox.className = "aemp-titlebox-closed";
+    } else {
+      this.titleBox.className = "aemp-titlebox-open";
+    }
   };
 
   closeTitleDetails = () => {
